d0df1d49ffb253540b06cfabca914c5984b25922
Md. Sifatullah <sifatullah35-617@diu.edu.bd>
2023-10-22 06:46:06 +0600

    dashboard done

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,9 +1,20 @@
+import 'package:agricos/utils/custom_widget/custom_text.dart';
 import 'package:agricos/utils/custom_widget/heading.dart';
+import 'package:agricos/utils/list.dart';
+import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class DashBoardScreen extends StatelessWidget {
+class DashBoardScreen extends StatefulWidget {
   const DashBoardScreen({super.key});
+
+  @override
+  State<DashBoardScreen> createState() => _DashBoardScreenState();
+}
+
+class _DashBoardScreenState extends State<DashBoardScreen> {
+  final TextEditingController countryController = TextEditingController();
+  String selectedCountry = '';
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +36,204 @@ class DashBoardScreen extends StatelessWidget {
               },
               child: Image.asset(
                 'assets/icons/Vector (6).png',
+              ),
+            ),
+          ),
+          //country picker here
+          Positioned(
+            top: 160.h,
+            child: Container(
+              height: 45.h,
+              width: 300.w,
+              padding: EdgeInsets.symmetric(horizontal: 20.w),
+              decoration: BoxDecoration(
+                color: Colors.lightGreenAccent,
+                borderRadius: BorderRadius.circular(20.r),
+              ),
+              child: TextFormField(
+                controller: countryController,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Select State/Country',
+                  hintStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  suffixIcon: InkWell(
+                    onTap: () {
+                      showCountryPicker(
+                        context: context,
+                        showPhoneCode: false,
+                        onSelect: (Country country) {
+                          countryController.text = country.displayName;
+                          print('Select country: ${country.displayName}');
+                        },
+                      );
+                    },
+                    child: Icon(
+                      Icons.arrow_drop_down_rounded,
+                      size: 50.sp,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 220.h,
+            child: Container(
+              width: 300.w,
+              height: 408.h,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: GridView.builder(
+                        shrinkWrap: true,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            childAspectRatio: 0.71,
+                            crossAxisSpacing: 10.sp,
+                            mainAxisSpacing: 10.sp),
+                        itemCount: homeList.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            padding: EdgeInsets.all(10.w),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.black,
+                                width: 1,
+                              ),
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20.r),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  height: 100.h,
+                                  width: 150.w,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20.r),
+                                    image: DecorationImage(
+                                      image: NetworkImage(
+                                        homeList[index]['image'],
+                                      ),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                CustomText(
+                                  text: homeList[index]['name'],
+                                  fontSize: 14.sp,
+                                ),
+                                CustomText(
+                                  text: 'Expert in:',
+                                  txtColor: Colors.green,
+                                  fontSize: 10.sp,
+                                ),
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.circle,
+                                      size: 5.sp,
+                                    ),
+                                    SizedBox(
+                                      width: 5.w,
+                                    ),
+                                    Expanded(
+                                      child: CustomText(
+                                        text: homeList[index]['exp1'],
+                                        fontSize: 10.sp,
+                                        txtAlign: TextAlign.start,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                homeList[index]['exp2'] == ''
+                                    ? Container()
+                                    : Row(
+                                        children: [
+                                          Icon(
+                                            Icons.circle,
+                                            size: 5.sp,
+                                          ),
+                                          SizedBox(
+                                            width: 5.w,
+                                          ),
+                                          Expanded(
+                                            child: CustomText(
+                                              text: homeList[index]['exp2'],
+                                              fontSize: 10.sp,
+                                              txtAlign: TextAlign.start,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                              ],
+                            ),
+                          );
+                        }),
+                  )
+                ],
+              ),
+            ),
+          ),
+
+          Positioned(
+              bottom: 20.h,
+              child: Container(
+                width: 320.w,
+                height: 41.h,
+                decoration: ShapeDecoration(
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(45),
+                  ),
+                  shadows: const [
+                    BoxShadow(
+                      color: Color(0x3F000000),
+                      blurRadius: 4,
+                      offset: Offset(0, 4),
+                      spreadRadius: 0,
+                    )
+                  ],
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10.w),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                          onPressed: () {},
+                          icon: Image.asset(
+                            'assets/icons/Vector (7).png',
+                          )),
+                      IconButton(
+                          onPressed: () {},
+                          icon: Image.asset(
+                            'assets/icons/Vector (8).png',
+                          )),
+                    ],
+                  ),
+                ),
+              )),
+          Positioned(
+            bottom: 40.h,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(
+                    side: BorderSide(color: Colors.lightGreenAccent)),
+                backgroundColor: Colors.white,
+                shadowColor: Colors.black,
+                elevation: 5,
+              ),
+              child: Icon(
+                Icons.search,
+                size: 30.sp,
+                color: Colors.black,
               ),
             ),
           ),
---
+++ lib/utils/list.dart
@@ -0,0 +1,74 @@
+List homeList = [
+  {
+    'id': 1,
+    'name': 'Bill Miller',
+    'image':
+        'https://network.expertisefinder.com/expert-profile-photo/bill-miller',
+    'exp1': 'Floricultural crop production',
+    'exp2': '',
+  },
+  {
+    'id': 2,
+    'name': 'David Lobell',
+    'image':
+        'https://network.expertisefinder.com/expert-profile-photo/david-lobell',
+    'exp1': 'Crop yield gaps',
+    'exp2': 'Environmental',
+  },
+  {
+    'id': 3,
+    'name': 'Helen Booker',
+    'image':
+        'https://network.expertisefinder.com/expert-profile-photo/helen-booker',
+    'exp1': 'Wheat',
+    'exp2': 'Agronomy',
+  },
+  {
+    'id': 4,
+    'name': 'Ronnie Coffman',
+    'image':
+        'https://network.expertisefinder.com/expert-profile-photo/ronnie-coffman',
+    'exp1': 'Wheat genetics and',
+    'exp2': 'breeding',
+  },
+  {
+    'id': 5,
+    'name': 'John Borden',
+    'image':
+        'https://network.expertisefinder.com/expert-profile-photo/john-borden',
+    'exp1': 'Floricultural crop production',
+    'exp2': '',
+  },
+  {
+    'id': 6,
+    'name': 'Scott R. Smedley',
+    'image':
+        'https://network.expertisefinder.com/expert-profile-photo/nadir-erbilgin',
+    'exp1': 'Chemical ecology',
+    'exp2': 'Plant-insect interactions',
+  },
+  {
+    'id': 7,
+    'name': 'Bill Miller',
+    'image':
+        'https://network.expertisefinder.com/expert-profile-photo/bill-miller',
+    'exp1': 'Floricultural crop production',
+    'exp2': '',
+  },
+  {
+    'id': 8,
+    'name': 'Bill Miller',
+    'image':
+        'https://network.expertisefinder.com/expert-profile-photo/bill-miller',
+    'exp1': 'Floricultural crop production',
+    'exp2': '',
+  },
+  {
+    'id': 9,
+    'name': 'Bill Miller',
+    'image':
+        'https://network.expertisefinder.com/expert-profile-photo/bill-miller',
+    'exp1': 'Floricultural crop production',
+    'exp2': '',
+  }
+];
